8eba331a382be9f05fb9a876c36709476e1c916b
Alex-a <alexrodrigoarruda@gmail.com>
2022-05-19 12:09:40 -0300

    Criando os botões

--- src/Main/Calculator.jsx
+++ src/Main/Calculator.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from "../Components/Button";
 
 import './Calculator.css';
 
@@ -6,7 +7,23 @@ export default function Calculator(props)   {
     return (
         <div>
             <div className="calculator">
-            
+                <Button label="AC" />
+                <Button label="÷" />
+                <Button label="7" />
+                <Button label="8" />
+                <Button label="9" />
+                <Button label="×" />
+                <Button label="4" />
+                <Button label="5" />
+                <Button label="6" />
+                <Button label="-" />
+                <Button label="1" />
+                <Button label="2" />
+                <Button label="3" />
+                <Button label="+" />
+                <Button label="0" />
+                <Button label="." />
+                <Button label="=" />
             </div>
         </div>
     );
